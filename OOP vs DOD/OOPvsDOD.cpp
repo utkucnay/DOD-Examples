fc1b94b2e15972343fbff054b7c54ee7041c1e27
utkucnay <48163717+utkucnay@users.noreply.github.com>
2022-08-15 15:25:48 +0300

    Update OOPvsDOD.cpp

--- OOP vs DOD/OOPvsDOD.cpp
+++ OOP vs DOD/OOPvsDOD.cpp
@@ -92,5 +92,5 @@ int main() {
     {
         apartLightData->lightData->at(i) = 5;
     }
-    printf("Set Light DOD Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+    printf("Set Light DOD Time taken: %.4fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 }
